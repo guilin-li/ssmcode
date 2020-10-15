7d40f9ee9bf1567060203b040f6022709d72f184
guilin@qq.com <guilin@qq.com>
2020-10-15 20:44:53 +0800

    整合mybatis

--- ssm01/src/main/java/com/lgl/dao/Impl/PersonDaoImpl.java
+++ ssm01/src/main/java/com/lgl/dao/Impl/PersonDaoImpl.java
@@ -7,14 +7,17 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository
+//  @Repository
+@Deprecated
 public class PersonDaoImpl implements IPersonDao {
     @Override
     public List<Person> findAll() {
         System.out.println("findAll dao");
         List<Person> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            list.add(new Person("jack"+i,100.00));
+            Person p = new Person("jack"+i,100.00);
+            p.setId(i);
+            list.add(p);
         }
         return list;
     }
